a771cb391aabaa5c11abac88daf56dc0dbf02d48
dom <davidedomini@gmail.com>
2022-03-26 08:27:42 +0100

    refactored LogicsImpl.scala, removed Pair abstraction in order to use Scala Tuples

--- src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
@@ -2,26 +2,19 @@ package u04lab.polyglot.a01a
 import Logics.*
 import scala.util.Random
 
-trait Pair:
-  def firstElement: Int
-  def secondElement: Int
 
 trait PairSet:
-  def addElement(e: Pair): Unit
+  def addElement(e: (Int, Int)): Unit
   def size(): Int
-
-object Pair:
-  def apply(firstElem: Int, secondElem: Int): Pair = PairImpl(firstElem, secondElem)
-  private case class PairImpl(override val firstElement: Int, override val secondElement: Int) extends Pair
 
 object PairSet:
   def apply(): PairSet = PairSetImpl()
 
   private class PairSetImpl() extends PairSet:
     import u04lab.code.List
-    private var elements: List[Pair] = List.Nil()
-    private def checkNotAlreadyPresent(e: Pair): Boolean = !List.contains(elements, e)
-    override def addElement(e: Pair): Unit = elements = if checkNotAlreadyPresent(e) then List.append(elements, List.Cons(e, List.Nil())) else elements
+    private var elements: List[(Int, Int)] = List.Nil()
+    private def checkNotAlreadyPresent(e: (Int, Int)): Boolean = !List.contains(elements, e)
+    override def addElement(e: (Int, Int)): Unit = elements = if checkNotAlreadyPresent(e) then List.append(elements, List.Cons(e, List.Nil())) else elements
     override def size(): Int = List.length(elements)
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01a/sol2/ */
@@ -37,7 +30,7 @@ class LogicsImpl(private val size: Int, private val boat: Int) extends Logics:
 
   def hit(row: Int, col: Int) =
     if(row == boatRow && col >= boatLeftCol && col < boatLeftCol + boat) then
-      hitsSet.addElement(Pair(row, col))
+      hitsSet.addElement((row, col))
       if(hitsSet.size() == boat) then Result.WON else Result.HIT
     else
       failures = failures + 1
